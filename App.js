af546e432087112cdf4ea4f76cd084ad35125a0c
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-06-27 17:21:06 +0200

    test

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image,  SafeAreaView } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    alert('You pressed the button!?');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={handlePress}>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/favicon.png')} />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
